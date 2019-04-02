d861e3e7421c1e0c373ed40e462928c034b6e0cc
Christopher Harrison <cnharrison@gmail.com>
2019-04-02 12:39:00 -0400

    put friends in state

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,11 +3,19 @@ import "./App.css";
 import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   componentDidMount = () => {
-    axios.get("http://localhost:5000/friends").then(response => {
-      console.log(response);
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({ friends: response.data });
     });
   };
+
   render() {
     return (
       <div className="App">
